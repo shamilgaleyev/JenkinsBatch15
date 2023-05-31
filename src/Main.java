307551192dcb833d07f62ad064e399d776985daf
shamilgaleyev <galeyevshamil@gmail.com>
2023-05-30 21:19:39 -0400

    change

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println("Hello world!");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         }
     }
 }
